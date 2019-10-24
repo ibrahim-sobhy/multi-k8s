d16a857bad76c0ca2a1e20b374105659ffa29d61
Ibrahim Freig <ibrahim.sobhy.cse07@gmail.com>
2019-10-24 12:16:49 +0200

    correct the docker hub name

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-docker build -t ibrahim-sobhy/multi-client:latest -t ibrahim-sobhy/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t ibrahim-sobhy/multi-server:latest -t ibrahim-sobhy/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t ibrahim-sobhy/multi-worker:latest -t ibrahim-sobhy/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push ibrahim-sobhy/multi-client:latest
-docker push ibrahim-sobhy/multi-server:latest
-docker push ibrahim-sobhy/multi-worker:latest
+docker build -t ibrahimsobhy/multi-client:latest -t ibrahimsobhy/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t ibrahimsobhy/multi-server:latest -t ibrahimsobhy/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t ibrahimsobhy/multi-worker:latest -t ibrahimsobhy/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push ibrahimsobhy/multi-client:latest
+docker push ibrahimsobhy/multi-server:latest
+docker push ibrahimsobhy/multi-worker:latest
 
-docker push ibrahim-sobhy/multi-client:$SHA
-docker push ibrahim-sobhy/multi-server:$SHA
-docker push ibrahim-sobhy/multi-worker:$SHA
+docker push ibrahimsobhy/multi-client:$SHA
+docker push ibrahimsobhy/multi-server:$SHA
+docker push ibrahimsobhy/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=ibrahim-sobhy/multi-server:$SHA
-kubectl set image deployments/client-deployment server=ibrahim-sobhy/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=ibrahim-sobhy/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=ibrahimsobhy/multi-server:$SHA
+kubectl set image deployments/client-deployment server=ibrahimsobhy/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=ibrahimsobhy/multi-worker:$SHA
